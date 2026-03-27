1509301d1c9cdfc1caa70dd5e183c0d2003ac9e2
MINJI, JUNG <105187829+alwaysgoodjmj@users.noreply.github.com>
2026-03-27 17:45:14 +0900

    Add files via upload
    gnb css 작업

--- js/swiper.js
+++ js/swiper.js
@@ -1,14 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const swiper = new Swiper('.slide-container', {
+    const mainSwiper = new Swiper('.main-slide', {
       loop: true,
       autoplay: {
           delay: 4000,
           disableOnInteraction: false,
       },
       pagination: {
-        el: '.swiper-pagination',
+        el: '.main-slide .swiper-pagination',
         clickable: true,
       }
+    });
+
+    const contentSwiper = new Swiper('.content-slide', {
+      slidesPerView: 3,
+      spaceBetween: 20,
+      centeredSlides: true,
+      navigation: {
+        nextEl: '.content-slide .swiper-button-next',
+        prevEl: '.content-slide .swiper-button-prev',
+      },
+      breakpoints: {
+        768: { slidesPerView: 3, spaceBetween: 30 },
+        1024: { slidesPerView: 4, spaceBetween: 40 }
+    }
     });
 }); 
   
